9cf492c812892e6e5ca7fe10b512d36432e7e022
carl <github@carljackson.com>
2014-03-31 17:14:57 -0700

    infinite scroll

--- src/com/carljackson/twitterapp/TimelineActivity.java
+++ src/com/carljackson/twitterapp/TimelineActivity.java
@@ -14,27 +14,25 @@ import com.carljackson.twitterapp.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 public class TimelineActivity extends Activity {
+	ArrayList<Tweet> tweets = new ArrayList<Tweet>();
+	TweetsAdapter adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_timeline);
-
-		TwitterClientApp.getRestClient().getHomeTimeline(new JsonHttpResponseHandler() {
-			@Override
-			public void onSuccess(JSONArray jsonTweets) {
-				ArrayList<Tweet> tweets = Tweet.fromJson(jsonTweets);
-				ListView lvTweets = (ListView)findViewById(R.id.lvTweets);
-				TweetsAdapter adapter = new TweetsAdapter(getBaseContext(), tweets);
-				lvTweets.setAdapter(adapter);
-
-			}
+		ListView lvTweets = (ListView) findViewById(R.id.lvTweets);
+		adapter = new TweetsAdapter(this, tweets);
+		lvTweets.setAdapter(adapter);
+		lvTweets.setOnScrollListener(new EndlessScrollListener() {
 
 			@Override
-			public void onFailure(Throwable throwable) {
-				Log.d("DEBUG", throwable.toString());
+			public void onLoadMore(int page, int totalItemsCount) {
+				loadMoreTweets();
 			}
 		});
+
+		loadMoreTweets();
 	}
 
 	@Override
@@ -42,6 +40,26 @@ public class TimelineActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.timeline, menu);
 		return true;
+	}
+
+	private void loadMoreTweets() {
+		long maxId = 0;
+		if (tweets.size() > 0) {
+			maxId = tweets.get(tweets.size() - 1).getId();
+		}
+		TwitterClientApp.getRestClient().getHomeTimeline(maxId, new JsonHttpResponseHandler() {
+			@Override
+			public void onSuccess(JSONArray jsonTweets) {
+				adapter.addAll(Tweet.fromJson(jsonTweets));
+			}
+
+			@Override
+			public void onFailure(Throwable throwable) {
+				Log.d("DEBUG", throwable.toString());
+			}
+		});
+
+
 	}
 
 }
--- src/com/carljackson/twitterapp/TwitterClient.java
+++ src/com/carljackson/twitterapp/TwitterClient.java
@@ -21,27 +21,30 @@ import com.loopj.android.http.AsyncHttpResponseHandler;
  * 
  */
 public class TwitterClient extends OAuthBaseClient {
-	public static final Class<? extends Api> REST_API_CLASS = TwitterApi.class; // Change this
-	public static final String REST_URL = "https://api.twitter.com/1.1"; // Change this, base API URL
-	public static final String REST_CONSUMER_KEY = "wNrQBNN8UR9xvPGLQS8gqw";       // Change this
-	public static final String REST_CONSUMER_SECRET = "0vgrFbmasGoXMMAmCNM0AVp7BuYfdge40SRH4WJJs"; // Change this
-	public static final String REST_CALLBACK_URL = "oauth://twitterapp"; // Change this (here and in manifest)
-
+	public static final Class<? extends Api> REST_API_CLASS = TwitterApi.class;
+	public static final String REST_URL = "https://api.twitter.com/1.1";
+	public static final String REST_CONSUMER_KEY = "wNrQBNN8UR9xvPGLQS8gqw";
+	public static final String REST_CONSUMER_SECRET = "0vgrFbmasGoXMMAmCNM0AVp7BuYfdge40SRH4WJJs";
+	public static final String REST_CALLBACK_URL = "oauth://twitterapp";
 	public TwitterClient(Context context) {
-		super(context, REST_API_CLASS, REST_URL, REST_CONSUMER_KEY, REST_CONSUMER_SECRET, REST_CALLBACK_URL);
+		super(context, REST_API_CLASS, REST_URL, REST_CONSUMER_KEY,
+				REST_CONSUMER_SECRET, REST_CALLBACK_URL);
 	}
 
-	public void getHomeTimeline(AsyncHttpResponseHandler handler) {
-		String url = getApiUrl("statuses/home_timeline.json");
-		client.get(url,  null, handler);
+	public void getHomeTimeline(long maxId, AsyncHttpResponseHandler handler) {
+		String url = getApiUrl("statuses/home_timeline.json?count=25");
+		if (maxId > 0) {
+			url += "&max_id=" + String.format("%d", maxId);
+		}
+		client.get(url, null, handler);
 	}
 
-	/* 1. Define the endpoint URL with getApiUrl and pass a relative path to the endpoint
-	 * 	  i.e getApiUrl("statuses/home_timeline.json");
-	 * 2. Define the parameters to pass to the request (query or body)
-	 *    i.e RequestParams params = new RequestParams("foo", "bar");
-	 * 3. Define the request method and make a call to the client
-	 *    i.e client.get(apiUrl, params, handler);
-	 *    i.e client.post(apiUrl, params, handler);
+	/*
+	 * 1. Define the endpoint URL with getApiUrl and pass a relative path to the
+	 * endpoint i.e getApiUrl("statuses/home_timeline.json"); 2. Define the
+	 * parameters to pass to the request (query or body) i.e RequestParams
+	 * params = new RequestParams("foo", "bar"); 3. Define the request method
+	 * and make a call to the client i.e client.get(apiUrl, params, handler);
+	 * i.e client.post(apiUrl, params, handler);
 	 */
 }
